253ae2ba5b2bb9bb316dabbe081200262d4459b2
Cj-bc <cj-bc@y-modify.org>
2019-05-08 10:15:29 +0900

    Add: sclack formula

---
+++ sclack.rb
@@ -0,0 +1,89 @@
+class Sclack < Formula
+  include Homebrew::Formula::Virtualenv
+  url "https://github.com/haskellcamargo/sclack.git"
+  head "https://github.com/haskellcamargo/sclack.git"
+  homepage "https://github.com/haskellcamargo/sclack"
+  desc "The best CLI client for Slack, because everuthing is terrible! (alpha)"
+  version "alpha"
+
+  depends_on "pipenv"
+  optional "libcaca"
+
+
+  resource "asyncio" do
+    url "https://files.pythonhosted.org/packages/da/54/054bafaf2c0fb8473d423743
+e191fcdf49b2c1fd5e9af3524efbe097bafd/asyncio-3.4.3.tar.gz"
+    sha256 "83360ff8bc97980e4ff25c964c7bd3923d333d177aa4f7fb736b019f26c7cb41"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/06/b8/d1ea38513c22e8c906275d13
+5818fee16ad8495985956a9b7e2bb21942a1/certifi-2019.3.9.tar.gz"
+    sha256 "b26104d6835d1f5e49452a26eb2ff87fe7090b89dfcaee5ea2212697e1e1d7ae"
+  end
+
+  resource "chardet" do
+    url "https://files.pythonhosted.org/packages/fc/bb/a5768c230f9ddb03acc9ef3f
+0d4a3cf93462473795d18e9535498c8f929d/chardet-3.0.4.tar.gz"
+    sha256 "84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/ad/13/eb56951b6f7950cadb579ca1
+66e448ba77f9d24efc03edd7e55fa57d04b7/idna-2.8.tar.gz"
+    sha256 "c357b3f628cf53ae2c4c05627ecc484553142ca23264e593d327bcde5e9c3407"
+  end
+
+  resource "pyperclip" do
+    url "https://files.pythonhosted.org/packages/33/15/f3c29b381815ae75e2758958
+3655f4a8567721c541b8ba8cd52f76868655/pyperclip-1.6.2.tar.gz"
+    sha256 "43496f0a1f363a5ecfc4cda5eba6a2a3d5056fe6c7ffb9a99fbb1c5a3c7dea05"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/52/2c/514e4ac25da2b08ca5a464c5
+0463682126385c4272c18193876e91f4bc38/requests-2.21.0.tar.gz"
+    sha256 "502a824f31acdacb3a35b6690b5fbf0bc41d63a24a45c4004352b0242707598e"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/dd/bf/4138e7bfb757de47d1f4b699
+4648ec67a51efe58fa907c1e11e350cddfca/six-1.12.0.tar.gz"
+    sha256 "d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73"
+  end
+
+  resource "slackclient" do
+    url "https://files.pythonhosted.org/packages/1f/9e/dd5bb327786be753e5d8de97
+6082eb714cb83abeb00f67f0646ba2e166ad/slackclient-1.2.1.tar.gz"
+    sha256 "e9de0c893e8c5473107f5927ae1e543d35246f0c834f5e86470b22b762211577"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/8a/3c/1bb7ef6c435dea026f06ed9f
+3ba16aa93f9f4f5d3857a51a35dfa00882f1/urllib3-1.24.3.tar.gz"
+    sha256 "2393a695cd12afedd0dcb26fe5d50d0cf248e5a66f75dbd89a3d4eb333a61af4"
+  end
+
+  resource "urwid" do
+    url "https://files.pythonhosted.org/packages/c7/90/415728875c230fafd13d1185
+12bde3184d810d7bf798a631abc05fac09d0/urwid-2.0.1.tar.gz"
+    sha256 "644d3e3900867161a2fc9287a9762753d66bd194754679adb26aede559bcccbc"
+  end
+
+  resource "urwid-readline" do
+    url "https://files.pythonhosted.org/packages/88/7d/39a36845725eb153b2637d32
+4c1a4fbb6d304e6ef4028a189ac1f246104f/urwid_readline-0.9.tar.gz"
+    sha256 "a8b17c10d720313121f3b5358e648f5cdbe5754e61e6eadcedfe82d5973705e6"
+  end
+
+  resource "websocket_client" do
+    url "https://files.pythonhosted.org/packages/c9/bb/8d3dd9063cfe0cd5d03fe6a1
+f74ddd948f384e9c1eff0eb978f3976a7d27/websocket_client-0.47.0.tar.gz"
+    sha256 "a453dc4dfa6e0db3d8fd7738a308a88effe6240c59f3226eb93e8f020c216149"
+  end
+
+
+  def install
+    virtualenv_install_with_resources
+  end
+end
